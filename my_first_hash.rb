9ff4c3acba5286864170113c77a34e312040aa51
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-25 02:39:52 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,6 +1,6 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
+  bio = {"Name" => "Junaid", "Age" => 27, "Sex" => "Male", "Location" => "U.S" }
   
 end
 
