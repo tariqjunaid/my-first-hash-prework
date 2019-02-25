c9a1d56539e7ce33ef22f3749a0a1a9bcd53cd4d
Junaid Tariq <junaidtariq_91@yahoo.com>
2019-02-25 02:50:13 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,8 +13,6 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-
-  # Look up the value of the "oil paintings" key in the shipping_manifest hash below
   shipping_manifest["oil paintings"]
 end
 
@@ -24,14 +22,7 @@ def adding
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
-  # add 2 muskets to the shipping_manifest hash below
   shipping_manifest["muskets"] = 2
-
-
-  # add 4 gun powder to the shipping_manifest hash below
   shipping_manifest["gun powder"] = 4
-
-
-  # return the shipping_manifest hash below
   shipping_manifest
 end
